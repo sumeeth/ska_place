ff1d83430932ec3886423fa4f4a3958daef92e1e
Sumeeth K Ambati <sumeeth.ambati@gmail.com>
2017-07-28 23:15:08 +1000

    added slick js carousel
    added slick js carousel

--- web/js/mainjs.js
+++ web/js/mainjs.js
@@ -9,4 +9,30 @@ $(document).ready(function () {
     $('#appbundle_location_roadNumber').attr("title", "Enter road or street number");
     $('#appbundle_location_roadNumber').tooltip();
     $('#currentDateTime').text(moment().format('LLLL'));
-});
+    $('.slikCarousel').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        // normal options...
+        infinite: false,
+        // the magic
+        responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 3,
+            infinite: true
+        }
+        }, {
+        breakpoint: 600,
+        settings: {
+        slidesToShow: 2,
+        dots: true
+        }
+        }, {
+        breakpoint: 300,
+        settings: "unslick" // destroys slick
+        }]
+    });   
+  });
